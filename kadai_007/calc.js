1529581cd89d36ce1eca86ae71113caa62274f64
Momoka Sakaue <159637497+momoka0626@users.noreply.github.com>
2024-03-26 14:38:36 +0900

    kadai_007をアップロード

---
+++ kadai_007/calc.js
@@ -0,0 +1,13 @@
+const num = 20;
+if (num % 3) {
+    console.log('3の倍数です');
+}
+else if (num % 5) {
+    console.log('5の倍数です');
+}
+if (num % 3 && num % 5) {
+    console.log('3と5の倍数です');
+}
+else {
+    console.log (num);
+}
